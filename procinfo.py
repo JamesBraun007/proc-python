27463ee4c3c8879302bf99605e6803f8d251801d
JamesBraun007 <roger.t.braun@gmail.com>
2024-05-28 18:51:50 -0400

    Update procinfo.py

--- procinfo.py
+++ procinfo.py
@@ -16,68 +16,7 @@ import time
 # If no pattern is given, it prints an error that a pattern is missing.
 #
 
- 
-# first check if pattern is present
-if len(sys.argv) < 2:
-    print("Error: a pattern is missing")
-
-elif len(sys.argv) == 4:
-
-    # printing every "secs" seconds
-    while True:
-        pattern = sys.argv[3]
-
-        # sending processes of all users with user names to text file
-        os.system("ps -ef > process_info.txt")
-
-        # need to parse text file and extract PID, PPID, User(UID), and CMD
-        # make sure to only retrieve PID that have CMD that contains pattern
-        info_file = open("process_info.txt", "r")
-
-        print("{:<8} {:<8} {:<12} {:<12} {:<10} {:<10} {:<10}".format("PID", "PPID", "CMD", "USER", "MEM", "CPU", "THREADS"))
-        for line in info_file:
-            if pattern in line:
-                info = line.split()
-                USER = info[0]
-                PID = info[1]
-                PPID = info[2]
-
-
-                # getting CPU info in stat file
-                cpu_file = open(f"/proc/{PID}/stat", "r")
-                cpu_string = cpu_file.read()
-                cpu_data = cpu_string.split()
-
-                # getting both user time and kernel time
-                cpu_time = int((int(cpu_data[13]) + int(cpu_data[14]))/100)
-                
-
-                # getting Threads and MEM info in status file
-                threads_MEM_file = open(f"/proc/{PID}/status", "r")
-                threads_MEM_data = threads_MEM_file.read()
-                
-                for info in threads_MEM_data.split("\n"):
-                    if info.startswith("Threads:"):
-                        threads = info.split()[1]
-                
-                    
-                    if info.startswith("VmRSS"):
-                        mem = int(info.split()[1])
-                        mem_mb = int(mem/1024)
-
-                # need to align this properly
-                print("{:<8} {:<8} {:<12} {:<12} {:<10} {:<10} {:<10}".format(PID, PPID, f"({pattern})", USER, f"{mem_mb}MB", f"{cpu_time}secs", f"{threads}Thr"))           
-
-        info_file.close()
-        cpu_file.close()
-        threads_MEM_file.close()
-        time.sleep(int(sys.argv[2]))
-        
-
-# otherwise, only print once
-else:
-    pattern = sys.argv[1]
-
+def retrieveInfo(pattern):
     # sending processes of all users with user names to text file
     os.system("ps -ef > process_info.txt")
 
@@ -117,10 +56,32 @@ else:
                     mem_mb = int(mem/1024)
 
             # need to align this properly
-            print("{:<8} {:<8} {:<12} {:<12} {:<10} {:<10} {:<10}".format(PID, PPID, f"({pattern})", USER, f"{mem_mb}MB", f"{cpu_time}secs", f"{threads}Thr"))           
+            print("{:<8} {:<8} {:<12} {:<12} {:<10} {:<10} {:<10}".format(PID, PPID, f"({pattern})", USER, f"{mem_mb}MB", f"{cpu_time}secs", f"{threads}Thr"))  
 
     info_file.close()
     cpu_file.close()
-    threads_MEM_file.close()               
-                    
+    threads_MEM_file.close()  
+
+def main():
+ 
+    # first check if pattern is present
+    if len(sys.argv) != 2 and len(sys.argv) != 4:
+        print("Error: a pattern is missing or there incorrect number of arguments")
+
+    elif len(sys.argv) == 4:
+
+        # printing every "secs" seconds
+        while True:
+            pattern = sys.argv[3]
+            retrieveInfo(pattern)
+            time.sleep(int(sys.argv[2]))
             
+
+    # otherwise, only print once
+    else:
+        pattern = sys.argv[1]
+        retrieveInfo(pattern)        
+      
+
+if __name__ == "__main__":
+    main()
